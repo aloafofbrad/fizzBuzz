9538ec870645f8e28a931ad25f84592fdc1ac3b6
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2020-02-10 23:01:52 -0800

    Redo commit
    Oops

---
+++ fizz buzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 1;i < 101;i++)
+    {
+        if (i % 3 == 0)
+            cout << "Fizz ";
+        if (i % 5 == 0)
+            cout << "Buzz";
+        if ((i % 3 != 0) && (i % 5 != 0))
+            cout << i;
+        cout << endl;
+    }
+}
